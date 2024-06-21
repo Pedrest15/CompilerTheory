39beca6e1b400e2336e47280481dfb47d00e1fe6
Pedrest15 <pedroandrade@usp.br>
2024-06-20 23:35:59 -0300

    detalhe na linha de declarar

--- assignment2/src/main.c
+++ assignment2/src/main.c
@@ -4,20 +4,17 @@
 #include "../header/open_close.h"
 #include "../header/parser.h"
 
-#define TRUE 1
-#define FALSE 0
-
+// O programa deve ser executado da forma ./compilet file_output.txt file_input.txt
 int main(int argc, char *argv[]) {
-    char filename_output[10] = "saida.txt";
-
     // Verifica se o arquivo de entrada foi fornecido como argumento
-    if (argc < 2) {
-        printf("Usage: %s <input_file>\n", argv[0]);
+    if (argc < 3) {
+        printf("compiler: fatal error: no input files\ncompilation terminated.\n");
         return 1;
     }
 
-    // Obtém o nome do arquivo de entrada a partir dos argumentos da linha de comando
-    char *filename_input = argv[1];
+    // Obtem o nome do arquivo de entrada e saída a partir dos argumentos da linha de comando
+    char *filename_output = argv[1];
+    char *filename_input = argv[2];
 
     // Abre o arquivo de entrada para leitura
     FILE* file = open_file(filename_input, FILE_READT);
